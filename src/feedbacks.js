a2263cf813c09d7893dbad9f179ddd2fe923346f
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2025-12-16 22:25:36 +1100

    feat: getOid value feedback

---
+++ src/feedbacks.js
@@ -0,0 +1,40 @@
+import { Regex } from '@companion-module/base'
+import snmp from 'net-snmp'
+
+export default async function (self) {
+	const feedbackDefs = {}
+	feedbackDefs['getOID'] = {
+		name: 'OID value',
+		type: 'value',
+		options: [
+			{
+				type: 'textinput',
+				label: 'OID',
+				id: 'oid',
+				default: '',
+				required: true,
+				useVariables: { local: true },
+				regex: Regex.SOMETHING,
+			},
+			{
+				type: 'checkbox',
+				label: 'DisplayString',
+				id: 'displaystring',
+				tooltip: 'Convert OctetString (array of numbers) to DisplayString (text)',
+				default: false,
+			},
+		],
+		callback: async (feedback, _context) => {
+			return self.oidValues.get(feedback.id) ?? null
+		},
+		subscribe: async (feedback, context) => {
+			await self.getOid(feedback.options.oid, '', feedback.options.displaystring, context, feedback.id)
+		},
+		learn: async (feedback, context) => {
+			await self.getOid(feedback.options.oid, '', feedback.options.displaystring, context, feedback.id)
+            return undefined
+		},
+	}
+
+	self.setFeedbackDefinitions(feedbackDefs)
+}
