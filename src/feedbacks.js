239187caf00297b791cb8947e1fbf2902f049673
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2025-12-18 08:20:01 +1100

    fix: use OID as key for oidValues.get(). Improvement: Log when OID value isn't cached

--- src/feedbacks.js
+++ src/feedbacks.js
@@ -1,25 +1,17 @@
-import { OidOption } from './actions.js'
+import { OidOption, DisplayStringOption } from './actions.js'
 
 export default async function (self) {
 	const feedbackDefs = {}
 	feedbackDefs['getOID'] = {
 		name: 'OID value',
 		type: 'value',
-		options: [
-			OidOption,
-			{
-				type: 'checkbox',
-				label: 'DisplayString',
-				id: 'displaystring',
-				tooltip: 'Convert OctetString (array of numbers) to DisplayString (text)',
-				default: false,
-			},
-		],
+		options: [OidOption, DisplayStringOption],
 		callback: async (feedback, _context) => {
 			if (!self.oidValues.has(feedback.options.oid)) {
+				self.log('info', `Feedback OID not cached yet for ${feedback.id}, retrieving: ${feedback.options.oid}`)
 				await self.getOid(feedback.options.oid, '', feedback.options.displaystring, null, feedback.id)
 			}
-			return self.oidValues.get(feedback.id) ?? null
+			return self.oidValues.get(feedback.options.oid) ?? null
 		},
 		subscribe: async (feedback, context) => {
 			await self.getOid(feedback.options.oid, '', feedback.options.displaystring, context, feedback.id)
